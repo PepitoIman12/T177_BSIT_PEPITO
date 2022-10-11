479cb3662f0b6d5f7713cb75e0f7829193ea7ac7
PepitoIman12 <115530608+PepitoIman12@users.noreply.github.com>
2022-10-11 17:32:23 +0800

    Add files via upload

---
+++ Hello_World-Pepito.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int num1 = 1050;
+
+float num2 = 3.14;
+
+  printf("Jesse Emmanuel L.Pepito\n I am 19 years old\n The value of num1 is %d",num1,);
+
+
+  return 0;
+
+
+
+}
